bee9465ab3fd9a2473a9907160188a1efc5c18c9
GuiDev45 <bortolettodev@gmail.com>
2026-01-07 22:06:34 -0300

    Aula 24 - operadores aritméticos

---
+++ Cursos/udemy-001/Fundamentais/aritmeticos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << 2 + 2 + 10 << endl;
+  cout << 2 - 2 << endl;
+  cout << 2 * 2 << endl;
+  cout << 2 / 2 << endl;
+  cout << 10 % 3 << endl;
+  cout << 10 + 3 * 7 << endl;
+  cout << (10 + 3) * 7 << endl;
+
+  return 0;
+}
